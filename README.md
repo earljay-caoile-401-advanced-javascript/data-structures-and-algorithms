980ccfff4d2d78251070e11d192b5b927c12c968
Earl Jay Caoile <unfie555@gmail.com>
2020-03-23 20:02:25 -0700

    importing linked list implementation and insertions from other repo

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Data structures and algorithms repo for Code Fellows JavaScript 401 course
 
 ### Challenges
 
-1. [Whiteboard Challenge 01: Reverse an Array](/code-challenges/401/arrayReverse/README.md "Whiteboard Challenge 01: Reverse an Array")
-2. [Whiteboard Challenge 02: Array Shift](/code-challenges/401/arrayShift/README.md "Whiteboard Challenge 02: Array Shift")
-3. [Whiteboard Challenge 03: Array Binary Search](/code-challenges/401/arrayBinarySearch/README.md "Whiteboard Challenge 03: Array Binary Search")
+1. [Whiteboard Challenge 01: Reverse an Array](/code-challenges/401/arrayReverse/README.md)
+2. [Whiteboard Challenge 02: Array Shift](/code-challenges/401/arrayShift/README.md)
+3. [Whiteboard Challenge 05:: Linked List Implementation](/data-structures/linkedList/README.md)
+4. [Whiteboard Challenge 06: Linked List Insertions](/code-challenges/401/linkedListInsertions/README.md)
---
+++ code-challenges/401/linkedListInsertions/linked-list-insertions.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const LinkedList = require('../../../data-structures/linkedList/linked-list.js');
+
+class LinkedListInsertions extends LinkedList {
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  insertBefore(value, newValue) {
+    if (this.head.value === value) {
+      this.insert(newValue);
+      return;
+    }
+
+    const newNode = new Node(newValue);
+    let current = this.head;
+    while (current.next) {
+      if (current.next.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        return;
+      }
+      current = current.next;
+    }
+
+    throw new Error('value does not exist in linked list');
+  }
+
+  insertAfter(value, newValue) {
+    const newNode = new Node(newValue);
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        return;
+      }
+      current = current.next;
+    }
+
+    throw new Error('value does not exist in linked list');
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+module.exports = LinkedListInsertions;
