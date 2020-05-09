3310c5aa177b5c01ae83beca32400bb8a0caf766
Earl Jay Caoile <unfie555@gmail.com>
2020-05-09 09:50:26 -0700

    fixed fizzbuzztree too because I also misunderstood this problem

--- code-challenges/401/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/401/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,33 +1,30 @@
 'use strict';
+const Node = require('../../../data-structures/trees/node.js');
 
-const fizzBuzzTree = (bTree) => {
-  if (bTree && bTree.root) {
-    fizzBuzzHelper(bTree.root);
+const fizzBuzzTree = (node) => {
+  if (!node) {
+    return null;
   }
 
-  return bTree;
-};
+  let newRoot;
+  if (typeof node.val === 'number') {
+    const canDivByThree = node.val % 3 === 0;
+    const canDivByFive = node.val % 5 === 0;
 
-const fizzBuzzHelper = (node) => {
-  if (node) {
-    if (typeof node.val === 'number') {
-      const canDivByThree = node.val % 3 === 0;
-      const canDivByFive = node.val % 5 === 0;
-
-      if (canDivByThree && canDivByFive) {
-        node.val = 'FizzBuzz';
-      } else if (canDivByThree) {
-        node.val = 'Fizz';
-      } else if (canDivByFive) {
-        node.val = 'Buzz';
-      } else {
-        node.val = node.val.toString();
-      }
+    if (canDivByThree && canDivByFive) {
+      newRoot = new Node('FizzBuzz');
+    } else if (canDivByThree) {
+      newRoot = new Node('Fizz');
+    } else if (canDivByFive) {
+      newRoot = new Node('Buzz');
+    } else {
+      newRoot = new Node(node.val.toString());
     }
-
-    fizzBuzzHelper(node.left);
-    fizzBuzzHelper(node.right);
   }
+
+  newRoot.left = fizzBuzzTree(node.left);
+  newRoot.right = fizzBuzzTree(node.right);
+  return newRoot;
 };
 
 module.exports = fizzBuzzTree;
