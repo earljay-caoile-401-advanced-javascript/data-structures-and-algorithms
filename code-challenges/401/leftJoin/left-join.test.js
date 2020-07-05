190c264f134f0fffead5ef23ee3b41c390b1a75b
Earl Jay Caoile <unfie555@gmail.com>
2020-07-04 18:48:12 -0700

    added two more tests

--- code-challenges/401/leftJoin/left-join.test.js
+++ code-challenges/401/leftJoin/left-join.test.js
@@ -26,4 +26,45 @@ describe('left join', () => {
       ['guide', 'usher', 'follow'],
     ]);
   });
+
+  it('can return key value pairs from left table when there are no match keys on right table', () => {
+    const leftHash = new Hashtable(50);
+    const rightHash = new Hashtable(50);
+
+    leftHash.add('fond', 'enamored');
+    leftHash.add('wrath', 'anger');
+    leftHash.add('diligent', 'employed');
+    leftHash.add('outfit', 'garb');
+    leftHash.add('guide', 'usher');
+
+    rightHash.add('bob', 'saget');
+    rightHash.add('kelly', 'clarkson');
+    rightHash.add('ryan', 'seacrest');
+    rightHash.add('jo', 'momma');
+    rightHash.add('bob', 'gunderson');
+
+    expect(leftJoin(leftHash, rightHash)).toMatchObject([
+      ['fond', 'enamored', null],
+      ['wrath', 'anger', null],
+      ['diligent', 'employed', null],
+      ['outfit', 'garb', null],
+      ['guide', 'usher', null],
+    ]);
+  });
+
+  it('can join tables of different lengths', () => {
+    const leftHash = new Hashtable(50);
+    const rightHash = new Hashtable(50);
+
+    leftHash.add('fond', 'enamored');
+    leftHash.add('wrath', 'anger');
+    leftHash.add('diligent', 'employed');
+    rightHash.add('diligent', 'idle');
+
+    expect(leftJoin(leftHash, rightHash)).toMatchObject([
+      ['fond', 'enamored', null],
+      ['wrath', 'anger', null],
+      ['diligent', 'employed', 'idle'],
+    ]);
+  });
 });
